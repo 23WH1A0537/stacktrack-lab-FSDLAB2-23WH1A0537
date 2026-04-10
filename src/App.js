48fe5104a0d3f41316c8adf064a3832a7fd09ec6
Nainika Dugyala <23wh1a0537@bvrithyderabad.edu.in>
2026-04-10 15:44:22 +0530

    routing config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-
+import QuestionPage from './pages/QuestionPage';
+import {fetchTaskById} from './api/client';
+import './App.css';
 function App() {
+  // useEffect(()=>{
+  //   const testApi = async()=>{
+  //     const data = await fetchTaskById("1");
+  //     console.log("Test API Response:",data);
+  //   };
+  //   testApi();
+  // },[]);
   return (
-    <div>
-      <Home />
-
-      {/* TODO: Replace this placeholder with Routes */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path = "/" element = {<Home />}/>
+        <Route path = "/task/:id" element={<QuestionPage />} />
+        
+      </Routes>
+    </Router>
+    // <div className = "App">
+    //   <h1> Testing API</h1>
+    // </div>
   );
 }
 
