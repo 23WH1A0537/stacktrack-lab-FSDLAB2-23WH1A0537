99b5d5fc3c03367c4d275e2aeedd4d7a48138de7
Nainika Dugyala <23wh1a0537@bvrithyderabad.edu.in>
2026-04-10 15:33:14 +0530

    API GET by id

--- src/api/client.js
+++ src/api/client.js
@@ -1,2 +1,16 @@
 // TODO: Set the base URL for your API
+export const fetchTaskById = async(id) =>{
+    try{
+        const response = await fetch('/api/tasks/${id}');
+        if (!response.ok){
+            throw new Error('No network response');
+        }
+        return await response.json();
+    }
+    catch(error){
+        console.error("failed",error);
+        return null;
+    }
+    
+};
 
