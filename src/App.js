964ee09a2353ae1fd3a4ee5b7d9835e1b7b787d8
Nainika Dugyala <23wh1a0537@bvrithyderabad.edu.in>
2026-04-10 16:11:45 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import QuestionPage from './pages/QuestionPage';
 import {fetchTaskById} from './api/client';
 import './App.css';
 function App() {
-  // useEffect(()=>{
-  //   const testApi = async()=>{
-  //     const data = await fetchTaskById("1");
-  //     console.log("Test API Response:",data);
-  //   };
-  //   testApi();
-  // },[]);
+  useEffect(()=>{
+    const testApi = async()=>{
+      const data = await fetchTaskById("1");
+      console.log("Test API Response:",data);
+    };
+    testApi();
+  },[]);
   return (
     <Router>
       <Routes>
         <Route path = "/" element = {<Home />}/>
         <Route path = "/task/:id" element={<QuestionPage />} />
-        
+
       </Routes>
     </Router>
     // <div className = "App">
